e50d76bd85e8190ceae9213834a74e7bf94ad51b
buddhika85 <buddhika2006system@gmail.com>
2015-08-20 12:58:59 +0100

    Images URLS

--- sample/scripts/custom/indexScript.js
+++ sample/scripts/custom/indexScript.js
@@ -3,7 +3,10 @@
 (function () {
     "use strict";
     
+    // get all the employees as a json object list
     var employees = GetEmployeeJsonObjs();
+
+    // drawing the JS data table
     DrawJqDataTable(employees);
 
     // used to draw JQ data table
@@ -20,7 +23,7 @@
                     {
                         "mData": "logo", "sTitle": "Logo", "mRender": function (data, type, row) {
                             if (data != null) {
-                                return '<img src = ' + data + ' style="height:18px;width:19px;"></img>';
+                                return '<img src = ' + data + ' ></img>';
                             }
                             else {
                                 return "No Logo to show";
@@ -40,14 +43,14 @@
         // you can include ajax or web service call to get the real employee objects from the server side
         // dummy data - hard coded
         var employees = [
-            { "id": 1, "name": "Jack Dawson", "joinDate": new Date(2011, 10, 30), "salary": 1500.5555, "departmentName": "Engineering", "logo": '../logos/engineering.jpg' },
-            { "id": 2, "name": "Kate Hamilton", "joinDate": new Date(2012, 12, 30), "salary": 1300.999, "departmentName": "Marketting", "logo": '../logos/marketting.jpg' },
-            { "id": 3, "name": "Sean Williams", "joinDate": new Date(2013, 1, 1), "salary": 1200.999, "departmentName": "Finance", "logo": '../logos/finance.jpg' },
-            { "id": 4, "name": "James Swayer", "joinDate": new Date(2013, 2, 1), "salary": 1300.999, "departmentName": "Human Resource", "logo": '../logos/humanResource.jpg' },
-            { "id": 5, "name": "Jack Siddle", "joinDate": new Date(2011, 10, 30), "salary": 1500.5555, "departmentName": "Engineering", "logo": '../logos/engineering.jpg' },
-            { "id": 6, "name": "Gill James", "joinDate": new Date(2012, 12, 30), "salary": 1300.999, "departmentName": "Marketting", "logo": '../logos/marketting.jpg' },
-            { "id": 7, "name": "Alex Starc", "joinDate": new Date(2013, 1, 1), "salary": 1200.999, "departmentName": "Finance", "logo": '../logos/finance.jpg' },
-            { "id": 8, "name": "Micheal Clark", "joinDate": new Date(2013, 2, 1), "salary": 1300.999, "departmentName": "Human Resource", "logo": '../logos/humanResource.jpg' }
+            { "id": 1, "name": "Jack Dawson", "joinDate": new Date(2011, 10, 30), "salary": 1500.5555, "departmentName": "Engineering", "logo": '../sample/logos/engineering.png' },
+            { "id": 2, "name": "Kate Hamilton", "joinDate": new Date(2012, 12, 30), "salary": 1300.999, "departmentName": "Marketting", "logo": '../sample/logos/marketting.png' },
+            { "id": 3, "name": "Sean Williams", "joinDate": new Date(2013, 1, 1), "salary": 1200.999, "departmentName": "Finance", "logo": '../sample/logos/finance.png' },
+            { "id": 4, "name": "James Swayer", "joinDate": new Date(2013, 2, 1), "salary": 1300.999, "departmentName": "Human Resource", "logo": '../sample/logos/humanResource.png' },
+            { "id": 5, "name": "Jack Siddle", "joinDate": new Date(2011, 10, 30), "salary": 1500.5555, "departmentName": "Engineering", "logo": '../sample/logos/engineering.png' },
+            { "id": 6, "name": "Gill James", "joinDate": new Date(2012, 12, 30), "salary": 1300.999, "departmentName": "Marketting", "logo": '../sample/logos/marketting.png' },
+            { "id": 7, "name": "Alex Starc", "joinDate": new Date(2013, 1, 1), "salary": 1200.999, "departmentName": "Finance", "logo": '../sample/logos/finance.png' },
+            { "id": 8, "name": "Micheal Clark", "joinDate": new Date(2013, 2, 1), "salary": 1300.999, "departmentName": "Human Resource", "logo": '../sample/logos/humanResource.png' }
         ];
         return employees;
     }
